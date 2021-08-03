afb1d8ec882f04c1afdbe3f25e179adb4be6f14f
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2021-08-02 23:55:37 -0300

    some things

---
+++ 1759_ho_ho_ho.py
@@ -0,0 +1,12 @@
+'''Problema:    1759 - Ho Ho Ho
+Resposta:    Accepted
+Linguagem:    Python 3.9 (Python 3.9.4) [+1s] {beta}
+Tempo:    0.000s
+Tamanho:    84 Bytes
+Memória:    -
+Submissão:    30/07/2021 23:59:47
+'''
+entrada = int(input())
+entrada -= 1
+frase = 'Ho ' * entrada + 'Ho!'
+print(frase)
