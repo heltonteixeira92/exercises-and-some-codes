ac3e03de43689644ad83887d9be875f72dde31de
Helton Teixeira de Souza <helton.souza@ongame.corp.com.br>
2022-07-04 15:36:37 -0300

    organizando em pasta

--- teste.py
+++
@@ -1,8 +0,0 @@
-List1 = (['apple', 'banana', 'blueberry', 'banana', 'blueberry', 'coconut', 'blueberry', 'coconut', 'lemon', 'mango'])
-List2 = (['melon', 'banana', 'pineapple', 'banana', 'melon', 'coconut', 'watermelon', 'coconut', 'watermelon', 'orange'])
-Common = []
-for element in List1:
-    if element in List2:
-        Common.append(element)
-
-print(Common)
