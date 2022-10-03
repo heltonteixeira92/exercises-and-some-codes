2624524dc242e61e2aebf6fc3d727c5296bb04c2
Helton Teixeira de Souza <helton.souza@ongame.corp.com.br>
2022-10-03 14:47:27 -0300

    linked list and hashtable

--- hashtable.py
+++ hashtable.py
@@ -1,21 +1,33 @@
-class Hashtable:
-    def __init__(self, items):
-        self.bucket_size = len(items)
-        self.buckets = [[i] for i in range(self.bucket_size)]
-        self.assign_buckets(items)
+class HashTable:
+    def __init__(self):
+        self.Max = 100
+        self.arr = [None for i in range(self.Max)]
 
-    def assign_buckets(self, items):
-        for key, value in items:
-            has_value = hash(key)
-            index = has_value % self.bucket_size
-            self.buckets[index].append((key, value))
+    def get_hash(self, key):
+        hash = 0
+        for char in key:
+            hash += ord(char)
+        return hash % self.Max
 
-    def get_value(self, input_keys):
-        has_value = hash(input_keys)
-        index = has_value % self.bucket_size
-        bucket = self.buckets[index]
-        for key, value in bucket:
-            if key == input_keys:
-                return value
+    def __getitem__(self, index):
+        h = self.get_hash(index)
+        return self.arr[h]
+
+    def __setitem__(self, key, val):
+        h = self.get_hash(key)
+        self.arr[h] = val
+
+    def __delitem__(self, key):
+        h = self.get_hash(key)
+        self.arr[h] = None
 
 
+t = HashTable()
+t['march 6'] = 310
+t["march 7"] = 420
+t["march 10"] = 420
+t["dec 30"] = 88
+
+del t["march 6"]
+print(t.arr)
+print(t["dec 30"])
